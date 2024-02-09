75bf34949f0b4431a4ddac74de77599fda7d7318
Grzegorz <blawik3@tlen.pl>
2024-02-09 09:00:14 +0100

    Show favorite currency

--- resources/views/components/favorite-currency-card.blade.php
+++ resources/views/components/favorite-currency-card.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full px-6 sm:w-1/2 xl:w-1/3">
+<div class="w-full px-6 sm:w-1/2 xl:w-1/3 mt-4">
 <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
     <div class="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-600 bg-opacity-75">
         <span class="text-white text-2xl font-bold">{{$letter}}</span>
